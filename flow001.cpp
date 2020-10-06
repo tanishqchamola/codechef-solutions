4ca36cf26691d08502f7e94ee6cfe0d4ef6705ef
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2020-10-06 13:24:43 +0530

    Some more solutions.

---
+++ flow001.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+/*
+int main() {
+    
+    unsigned int a,b;
+    int t;
+    cin>>t;
+    unsigned int result[t];
+    
+    for (int i=0;i<t;i++) {
+        cin>>a>>b;
+        unsigned int sum = 0;
+        sum = a+b;
+        
+        result[i] = sum;
+    }
+    
+    for(int j=0; j<t;j++)
+    {
+        cout<<result[j]<<"\n";
+    }
+    
+    return 0;
+}
+ */
---
+++ intest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/*
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,k;
+    int count =0;
+    cin>>n>>k;
+    
+    for (int i=0; i<n; i++) {
+        unsigned int num;
+        cin>>num;
+        
+        if(num%k==0)
+        {
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}*/
