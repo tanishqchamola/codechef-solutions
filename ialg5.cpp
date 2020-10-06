48b0a8b7f064adc9014bf5164a89a89b9edf54f0
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2020-10-06 13:26:16 +0530

    New practice solutions.

---
+++ ialg5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/*
+int main()
+{
+    cout<<"sdfsdfsdfs";
+    unsigned long long int n,k,r;
+    unsigned long long int i=0;
+    
+    cin>>n>>k>>r;
+    
+    n=n+1;
+    
+    while(k>=(r*i))
+    {
+        n--;
+        k=k-(r*i);
+        i++;
+    }
+    
+    cout<<n;
+    
+    return 0;
+}*/
---
+++ isitcake.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+int main()
+{
+    int t;
+    cin>>t;
+    vector<int>result;
+    
+    for (int i=0; i<t; i++)
+    {
+        int arr[10][10];
+        int count=0;
+        
+        for (int j=0; j<10; j++)
+        {
+            for (int k=0; k<10; k++)
+            {
+                cin>>arr[j][k];
+                
+                if(arr[j][k]<=30)
+                {
+                    count++;
+                }
+            }
+        }
+        
+        if (count>=60)
+        {
+            result.push_back(1);
+        }
+        else
+        {
+            result.push_back(0);
+        }
+    }
+    
+    for (int k=0;k<t;k++)
+    {
+        if(result[k] == 1)
+        {
+            cout<<"yes"<<"\n";
+        }
+        else
+        {
+            cout<<"no"<<"\n";
+        }
+    }
+}*/
