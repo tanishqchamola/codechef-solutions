45f08091e1669353f79906c13d233f4fee55139d
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2020-10-06 13:18:03 +0530

    Practice solutions.

---
+++ chfintro.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+/*
+int main()
+{
+    int n,r;
+    cin>>n;
+    int arr[n];
+    cin>>r;
+    
+    for(int i=0; i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    for (int j=0; j<n; j++)
+    {   int a = arr[j];
+        if(a >= r)
+        {
+            cout<<"Good boi"<<"\n";
+        }
+        else
+        {
+            cout<<"Bad boi"<<"\n";
+        }
+    }
+    return 0;
+}
+*/
