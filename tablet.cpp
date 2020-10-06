20a162708730f824b6921dc087ebf257c7292557
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2020-10-06 13:27:52 +0530

    New contribution.

---
+++ tablet.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+int main()
+{
+    vector <int> result;
+    unsigned short int t=0;
+    cin>>t;
+    
+    for (unsigned short int y=0; y<t; y++)
+    {
+        unsigned int n,b;
+        unsigned int max=0;
+        cin>>n>>b;
+        unsigned int arr[n][3];
+        
+        for (unsigned int i=0; i<n; i++)
+        {
+            cin>>arr[i][0]>>arr[i][1]>>arr[i][2];
+            
+            if (arr[i][2]<=b)
+            {
+                if ((arr[i][0]*arr[i][1])>max)
+                {
+                    max=(arr[i][0]*arr[i][1]);
+                }
+            }
+        }
+        
+        if (max==0)
+        {
+            result.push_back(0);
+        }
+        else{
+            result.push_back(max);
+        }
+    }
+    
+    for (unsigned short int p=0; p<t; p++)
+    {
+        if (result[p] == 0)
+        {
+            cout<<"no tablet\n";
+        }
+        else{
+            cout<<result[p]<<"\n";
+        }
+    }
+    
+    return 0;
+}
+*/
